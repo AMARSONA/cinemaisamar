0bd5bfeb33841f88b488b1357db16c8fcf65c658
AMAR SONA <40853239+AMARSONA@users.noreply.github.com>
2025-06-28 17:36:34 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -48,7 +48,7 @@ const Navbar = () => {
                                 <Link href={`/${session.user.name}`} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Your Page</Link>
                             </li>
                             <li>
-                                <Link onClick={() => signOut()} href={"/"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
+                                <Link onClick={() => signOut()} href={"/login"} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</Link>
                             </li>
                         </ul>
                     </div>
