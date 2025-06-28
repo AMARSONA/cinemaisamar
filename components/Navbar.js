feed6d61e5b1d1fe19ac4a5f52884599da3099e3
AMAR SONA <40853239+AMARSONA@users.noreply.github.com>
2025-06-28 17:03:04 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -61,7 +61,7 @@ const Navbar = () => {
           font-medium rounded-lg text-sm px-5 py-2.5 
           text-center me-2 mb-2'>Dashboard</button></Link>)} */}
 
-                {session && <button className='text-white w-fit bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={() => { signOut() }}>Logout</button>}
+                {session && <button className='text-white w-fit bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={() => { signOut({callbackUrl: "/"}) }}>Home</button>}
                 {!session && <Link href={"/login"}>
                      <button className='text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>}
                     {/* <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>} */}
