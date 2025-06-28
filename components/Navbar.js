0e20758be4cd5bc5a52bc0d5596ddb19c9b12019
AMAR SONA <40853239+AMARSONA@users.noreply.github.com>
2025-06-28 18:26:37 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -32,7 +32,7 @@ const Navbar = () => {
                         setTimeout(() => {
                             setShowdropdown(false)
                         }, 100);
-                    }} data-dropdown-toggle="dropdown" className=" mx-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 
+                    }} data-dropdown-toggle="dropdown" className=" mx-4 text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 
                     text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Account {/*Welcome session.user.email*/} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                         </svg>
