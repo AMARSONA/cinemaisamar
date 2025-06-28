8e8e727581deee4b210caccc77ee5ca5383a108e
AMAR SONA <40853239+AMARSONA@users.noreply.github.com>
2025-06-28 18:33:34 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -13,11 +13,11 @@ const Navbar = () => {
     //     </>
     // }
     return (
-        <nav className='text-black flex justify-between items-center px-4 md:h-16'>
+        <nav className=' text-black flex justify-between items-center px-4 md:h-16'>
 
             <Link href={"/"} className='logo font-bold text-lg flex justify-center items-center'>
                 <img className='invertImg' src="/cinema.gif" width={44} alt="" />
-                <span className='text-xl md:text-base my-3 md:my-0 text-white' >CinemaisAmar</span></Link>
+                <span className='text-xl md:text-base my-3 md:my-0 text-white'>CinemaisAmar</span></Link>
             {/* <ul className='flex justify-between gap-4'>
                 <li>Home</li>
                 <li>About</li>
@@ -33,7 +33,7 @@ const Navbar = () => {
                             setShowdropdown(false)
                         }, 100);
                     }} data-dropdown-toggle="dropdown" className=" mx-4 text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 
-                    text-center inline-flex items-center dark:focus:ring-blue-800" type="button">Account {/*Welcome session.user.email*/} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    text-center inline-flex items-center  dark:focus:ring-blue-800" type="button">Account {/*Welcome session.user.email*/} <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                         </svg>
                     </button>
@@ -61,7 +61,7 @@ const Navbar = () => {
           font-medium rounded-lg text-sm px-5 py-2.5 
           text-center me-2 mb-2'>Dashboard</button></Link>)} */}
 
-                {session && <button className='text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={() => { signOut({callbackUrl: "/"}) }}>Home</button>}
+                {session && (<button className='text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={() => { signOut( { callbackUrl: "/" })}}>New Login</button>)}
                 {!session && <Link href={"/"}><button className='text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Home</button></Link>}
                 {!session && <Link href={"/login"}><button className='text-white bg-transparent border-2 hover:bg-gray-400 hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>}
                     {/* <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>} */}
